3221a501280896c7f9534c95991d01523d682046
Sebastian Schepis <sschepis@gmail.com>
2017-12-09 17:55:31 -0700

    added new method for updating alias data, added syscoin core

--- config.js
+++ config.js
@@ -5,7 +5,9 @@ config.port = process.env.PORT || 3000;
 //NOTE: SECURE INFO SHOULD NOT BE COMMITTED TO PUBLIC GIT
 //mongodb config
 config.mongodb = {
-  database_url: process.env.MONGODB_URL || ``
+  database_url: process.env.MONGODB_URL || `mongodb://localhost`
 };
+
+config.sys_location = process.env.SYS_LOCATION || '/home/sebastian/.syscoin/';
 
 module.exports = config;
--- index.js
+++ index.js
@@ -2,19 +2,74 @@ const express = require('express');
 const bodyParser= require('body-parser');
 const app = express();
 const MongoClient = require('mongodb').MongoClient;
-const ObjectId = require('mongodb').ObjectId;
 const cors = require('cors');
+const SyscoinClient = require('syscoin-core');
+const syscoinAuth = require('syscoin-auth');
+const fs = require('fs');
 
 const config = require('./config');
-let db;
+let db, syscoinClient, rpcuser = "u", rpcpass = "p", rpcport = 8336;
 
 MongoClient.connect(config.mongodb.database_url, (err, database) => {
   if (err) return console.log(err);
 
   console.log("Database connection success.");
   db = database;
-  initApp();
+  initSyscoinClient();
 });
+
+function initSyscoinClient() {
+
+  let inputStreamError = false;
+  let inputStream = fs.createReadStream(config.sys_location + "syscoin.conf");
+  inputStream.on('error', function (e) {
+    console.log("Error reading syscoin.conf specified at " + config.sys_location + " falling back to defaults. Exact error is:" + JSON.stringify(e));
+    console.log("Syscoin.conf must be present, with rpcuser, rpcpass, and rpcport set in order to run this server.");
+    process.exit();
+  });
+
+  if(!inputStreamError) {
+    let lineReader = require('readline').createInterface({
+      input: inputStream
+    });
+
+    //read syscoin.conf for login creds, if it doesn't exist use defaults.
+    lineReader.on('line', function (line) {
+      if (line.indexOf('rpcuser=') === 0) {
+        rpcuser = line.substr(line.indexOf('=') + 1);
+      }
+
+      if (line.indexOf('rpcpassword=') === 0) {
+        rpcpass = line.substr(line.indexOf('=') + 1);
+      }
+
+      if (line.indexOf('rpcport=') === 0) {
+        rpcport = line.substr(line.indexOf('=') + 1);
+      }
+    });
+
+    //init SYS API on close of config file read
+    lineReader.on('close', function (line) {
+      initAPI();
+    });
+  }
+
+  function initAPI() {
+    console.log("RPCUSER:", rpcuser);
+    console.log("RPCPASS:", rpcpass);
+    console.log("RPCPORT:", rpcport);
+
+    syscoinClient = new SyscoinClient({
+      host: 'localhost',
+      port: rpcport,
+      username: rpcuser,
+      password: rpcpass,
+      timeout: 30000
+    });
+
+    initApp();
+  }
+}
 
 function initApp() {
   app.use(bodyParser.json());
@@ -32,30 +87,68 @@ function initApp() {
   });
 
   app.get('/', (req, res) => {
-    res.send('Fetch server operational.');
+    res.send('Proxy server operational.');
   });
 
-  app.get('/aliasdata/:id', (req, res) => {
+  app.get('/aliasdata/:aliasname', (req, res) => {
     let collection = db.collection('aliasdata');
-    let id = req.params.id;
+    let aliasName = req.params.aliasname;
 
-    console.log(`Searching for id ${id}`);
+    console.log(`Searching for alias ${aliasName}`);
 
     try {
-      collection.findOne({_id: ObjectId(id)}, (err, item) => {
+      collection.findOne({aliasName: aliasName}, (err, item) => {
         if (err) res.send(`Error with request: ${err}`);
 
         if (item) {
-          console.log(`Found result for ${id}`);
+          console.log(`Found result for ${aliasName}`);
           res.send(JSON.stringify(item));
         } else {
-          console.log(`No record found for ${id}`);
-          res.send(`No matching records for ${id}`);
+          console.log(`No record found for ${aliasName}`);
+          res.send(`No matching records for ${aliasName}`);
         }
       });
     }catch(e) { //catch errors related to invalid id formatting
       res.send(`Error with request: ${e}`);
     }
   });
+
+  app.post('/aliasdata/:aliasname', (req, res) => {
+    const collection = db.collection('aliasdata');
+    const aliasName = req.params.aliasname;
+    const aliasData = JSON.parse(req.body.payload);
+
+    const hashVerified = syscoinAuth.verifyHash(req.body.payload, req.body.hash);
+    if(!hashVerified) {
+      console.log(`Hashes do not match for ${aliasName}`);
+      return res.send(`Hashes do not match for ${aliasName}`);
+    }
+
+    syscoinClient.aliasInfo(aliasName).then((result) => {
+      if(!result || !result.address) {
+        console.log(`Invalid alias ${aliasName}`);
+        return res.send(`Invalid alias ${aliasName}`);
+      }
+      const sigVerified = syscoinAuth.verifySignature(
+        req.body.hash,
+        req.body.signedHash,
+        result.address
+      );
+      if(!sigVerified) {
+        console.log(`Signature verification failed for ${aliasName}`);
+        return res.send(`Signature verification failed for ${aliasName}`);
+      }
+      try {
+        collection.updateOne({ aliasName: aliasName }, aliasData, { upsert: true }, (err, result) => {
+          if (err) res.send(`Error with request: ${err}`);
+          res.send(JSON.stringify(result));
+        });
+      }catch(e) { //catch errors related to invalid id formatting
+        res.send(`Error with request: ${e}`);
+      }
+    });
+
+  });
+
 }
 
